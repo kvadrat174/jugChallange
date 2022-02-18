e431279777631b1b71bba9fc80d815e713d52f77
aosokin <andrey.osokin74@gmail.com>
2022-02-18 18:53:12 +0300

    typing error in nginx conf

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-    .setTitle('jugChallange')
+    .setTitle('jugChallenge')
     .setVersion('1.0')
     .build();
 
